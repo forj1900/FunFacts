3595c84e73801f51e50c4c7132364af41661274e
Sergey Zhuchkov <ser-zhuchkov@yandex.ru>
2015-11-15 00:33:29 +0300

    Initial Commit

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Sergey Zhuchkov on 15.11.15.
+//  Copyright © 2015 Sergey Zhuchkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
